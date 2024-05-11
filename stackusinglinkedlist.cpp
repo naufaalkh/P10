3d1a3acaf9edbd87cac1fb11ab1d7f8e5a5f84cc
naufaalkh <naufaalkh@gmail.com>
2024-05-11 12:14:29 +0700

    membuat push operation

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -20,4 +20,15 @@ class Stack {
     Stack() {
         top = NULL;
     }
+
+    int push(int value)
+    {
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "Push value: " << value << endl;
+        return value;
+    }
+    
 };
